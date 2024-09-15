9dcd42a9bdda91a773c26205e853d5baea65eacf
Mitja Šiška <mitja.siska@gmail.com>
2024-09-15 22:12:47 +0200

    Changed drizzle migrations folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'drizzle-kit';
 import * as process from 'node:process';
 
 export default defineConfig({
-  schema: './src/drizzle/schema/**/schema.ts',
   dialect: 'postgresql',
+  schema: './src/drizzle/schema/**/schema.ts',
+  out: './drizzle/migrations',
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
