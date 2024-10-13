8ba9d6416f7b1342829baccb59f9cefab06527cf
Mitja Šiška <mitja.siska@gmail.com>
2024-10-13 21:07:34 +0200

    Sort the endpoints in the Swagger documentation alphabetically

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,12 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
 
   await app.listen(3000);
 }
